b9cd6f7a92ea27bc6d98bd08a45a978e2835c519
Felipe Catalán <felipe.catalan@gatblac.com>
2022-11-16 11:21:49 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  /** 
+   * Trabajar con template, permite trabajar todo junto
+   * 
+  template: `
+    <h1> Hola de Nuevo</h1>
+  `, 
+  */
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  
 }
